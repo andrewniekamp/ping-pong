3947526491a93db993af77ec961fa013d00cbbdd
Andrew <andrew.f.niekamp@gmail.com>
2016-06-24 13:47:47 -0700

    update some styling and improve form control

--- js/scripts.js
+++ js/scripts.js
@@ -27,10 +27,16 @@ $(function() {
       $("#result").html(" ");
       $("#result").css("opacity", "0.01");
       $("#result").animate({opacity: 1}, 1500);
-      $("#result").html("<h1>Are u messing with me?</h1><p>Please try again...</p>");
+      $("#result").html("<h1>That number is too low!</h1><p>Please use a positive number...</p>");
+    } else if (rangeInput > 10000) {
+      $("#result").html(" ");
+      $("#result").css("opacity", "0.01");
+      $("#result").animate({opacity: 1}, 1500);
+      $("#result").html("<h1>That number is too high!</h1><p>Please use a number lower than 10,000...</p>");
     } else {
       pingPong(rangeInput);
-      outputItem = '';
+      outputItem = "<h1>Here you are!</h1>"
+      outputItem += '<ul>';
       for (var i = 0; i < result.length; i++) {
         outputItem += "<li>" + result[i] + "</li>";
       };
