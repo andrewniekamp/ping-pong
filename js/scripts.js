13c696cc3d086af0f1a420cdaec11450e4c2c97b
Andrew <andrew.f.niekamp@gmail.com>
2016-06-24 09:35:06 -0700

    store input from the user and pass it to a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
+//business logic
+var rangeInput = 0;
+
+var pingPong = function(upperRange) {
+  console.log(result);
+};
+
+//interface logic
 $(function() {
   $("#num-form").submit(function(event) {
     event.preventDefault();
-    
+    rangeInput = parseInt($("#range").val());
+    pingPong(rangeInput);
   })
 });
