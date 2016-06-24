e09addfc047656ad4412ebc5f41119fa430266d9
Andrew <andrew.f.niekamp@gmail.com>
2016-06-24 11:40:12 -0700

    add more styling and an error message if the user enter a negative number or zero

--- js/scripts.js
+++ js/scripts.js
@@ -24,14 +24,21 @@ $(function() {
   $("#num-form").submit(function(event) {
     event.preventDefault();
     rangeInput = parseInt($("#range").val());
-    pingPong(rangeInput);
-    outputItem = '';
-    for (var i = 0; i < result.length; i++) {
-      outputItem += "<li>" + result[i] + "</li>";
-    };
-    outputItem += "</ul>";
-    $("#result").html(outputItem);
-    $("#result").css("opacity", "0.01");
-    $("#result").animate({opacity: 1}, 1500);
+    if (rangeInput < 1) {
+      $("#result").html(" ");
+      $("#result").css("opacity", "0.01");
+      $("#result").animate({opacity: 1}, 1500);
+      $("#result").html("<h1>Are u messing with me?</h1><p>Please try again...</p>");
+    } else {
+      pingPong(rangeInput);
+      outputItem = '';
+      for (var i = 0; i < result.length; i++) {
+        outputItem += "<li>" + result[i] + "</li>";
+      };
+      outputItem += "</ul>";
+      $("#result").html(outputItem);
+      $("#result").css("opacity", "0.01");
+      $("#result").animate({opacity: 1}, 1500);
+    }
   });
 });
